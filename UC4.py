ded63c1c60fd7685b20435b8b55d151637206127
edsonapf <edsonalves@cc.ci.ufpb.br>
2018-04-06 16:12:45 -0300

    Add files via upload

--- UC4.py
+++ UC4.py
@@ -17,39 +17,44 @@ pc = ir + 1
 ciclos = 0
 opcao = 0
 parametros = []
+reg = []
 
 def Busca_Memoria(indice):
     return mem[indice]
 
-def Coloca_Memoria(indice):
-    return mem[indice]
+
+def Pega_Segundo_Parametro(arg):
+
+    if arg.isalpha():
+        parametros.append(arg)
+
+    elif arg[0] == '[':
+        MAR = arg.split('[')
+        MAR = MAR[1].split(']')
+        MAR = int(MAR[0])
+        parametros.append(MAR)
+
+    else:
+        parametros.append(arg)
+
 
 def Separa_Instrucao(arg):
 
     if arg[0][0].isalpha():  # verifica se a primeira posição eh uma letra
         parametros.append(arg[0])
+        if len(arg) == 2:
+            Pega_Segundo_Parametro(arg[1])
 
     elif arg[0][0] == '[':  # verifica se a primeira posição eh um colchete
 
         # tira os colchetes do parâmetro
-        MAR = arg[0][0].split('[')
+        MAR = arg[0].split('[')
         MAR = MAR[1].split(']')
         MAR = int(MAR[0])
         parametros.append(MAR)
 
-    # Compara o segundo parâmetro da instrução
-    elif arg[1][0].isalpha():
-        parametros.append(arg[1])
-
-    elif arg[1][0] == '[':
-        MAR = arg[1][0].split('[')
-        MAR = MAR[1].split(']')
-        MAR = int(MAR[0])
-        MBR = Busca_Memoria(MAR)
-        parametros.append(MBR)
-
-    elif arg[1][0].isnumeric():
-        parametros.append(int(arg[1]))
+        if len(arg) == 2:
+            Pega_Segundo_Parametro(arg[1])
 
     else:
         print('Erro de sintaxe!')
@@ -93,6 +98,33 @@ def Decodifica_Instrucao(instrucao):
         Separa_Instrucao(argumentos)
         opcao = 4
 
+    elif instrucao[0] == 'JE':
+
+        Separa_Instrucao(instrucao[1])
+        opcao = 5
+
+    elif instrucao[0] == 'JL':
+
+        Separa_Instrucao(instrucao[1])
+        opcao = 6
+
+    elif instrucao[0] == 'JG':
+
+        Separa_Instrucao(instrucao[1])
+        opcao = 7
+
+    elif instrucao[0] == 'MUL':
+        argumentos = instrucao[1].split(',')
+
+        Separa_Instrucao(argumentos)
+        opcao = 8
+
+    elif instrucao[0] == 'DIV':
+        argumentos = instrucao[1].split(',')
+
+        Separa_Instrucao(argumentos)
+        opcao = 9
+
 
     else:
         x=1
@@ -105,19 +137,19 @@ def Executa_Instrucao():
     global AL,prontapraexec,ula,acc,opcao,ciclos
 
     if opcao == 1:
-        if parametros[0] == 'AL':
+        """if parametros[0] == 'AL':
              AL = parametros[1]
         print('AL = {}'.format(AL))
         prontapraexec = 0
-        mov = 0
-        ciclos += 1
+        ciclos += 1"""
+        if parametros[0][0] == 'R':
+
 
     if opcao == 2:
         #ula = oper1+oper2
         print('Valor da Soma = {}'.format(ula))
         acc=ula
         prontapraexec = 0
-        add=0
         ciclos += 1
 
     #Para apagar os parâmetros da instruções que foi executada
